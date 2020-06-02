f2b97ae559ef7fea6c382088f9c95ca2889bb7a6
Detark333 <55380480+Detark333@users.noreply.github.com>
2020-06-02 14:26:23 +0400

    Update RAM.java

--- Lab3/src/memory/RAM.java
+++ Lab3/src/memory/RAM.java
@@ -9,8 +9,29 @@ public class RAM {
 	public RAM() {
 		table = new TableRecord[VIRTUAL_PAGES.getValue()];
 		frames = new boolean[MEMORY_FRAMES.getValue()];
+		generatePageTable();
 	}
-
+	public void generatePageTable() {
+		table = new TableRecord[VIRTUAL_PAGES.getValue()];
+		for (int i = 0; i < table.length; i++) {
+			TableRecord record = new TableRecord();
+			table[i] = record;
+		}
+	}
+	public void getPage(int index) {
+		if (index >= 0 && index < VIRTUAL_PAGES.getValue()) {
+			System.out.println("Page " + index + " loaded from disk");
+		} else {
+			throw new IllegalArgumentException("Index of of range");
+		}
+	}
+	public void updatePage(int index) {
+		if (index >= 0 && index < VIRTUAL_PAGES.getValue()) {
+			System.out.println("Page " + index + " updated on disk");
+		} else {
+			throw new IllegalArgumentException("Index of of range");
+		}
+	}
 	public TableRecord getTableRecord(int virtualPage) {
 		if (virtualPage >= 0 && virtualPage < VIRTUAL_PAGES.getValue()) {
 			return table[virtualPage];
@@ -41,7 +62,9 @@ public class RAM {
 	public TableRecord[] getPageTable() {
 		return table;
 	}
-
+	public TableRecord[] getData() {
+		return table;
+	}
 	public void removePage(int index) {
 		if (index >= 0 && index < MEMORY_FRAMES.getValue()) {
 			System.out.println("Page " + index + " removed from RAM");
